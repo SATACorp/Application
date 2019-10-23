a233147ff79d9d4826759beceec54f0da10dcd0c
pennert-1630865 <pennert@uw.edu>
2019-10-22 17:09:01 -0700

    Quizlist component typo renamed

--- src/components/QuizList/Quizlist.js
+++ src/components/QuizList/Quizlist.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./QuizList.module.css";
 
-export default function Quizlist() {
+export default function QuizList() {
   return <div className={styles.container}>QuizList</div>;
 }
