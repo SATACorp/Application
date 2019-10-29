48f17efc94cf668c745a57096834843f250b2787
pennert-1630865 <pennert@uw.edu>
2019-10-29 14:43:19 -0700

    added firebase comment in index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
 
+// UserModel, LoginModel, LeaderboardModel, and QuizModel initialized
+// through Firebase  that will be initialized here.
+
 ReactDOM.render(<App />, document.getElementById("root"));
