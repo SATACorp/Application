39df6e95f3af2d21fe60798e8697ffc1e1e60919
pennert-1630865 <pennert@uw.edu>
2019-11-10 20:51:34 -0800

    .env file added

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/auth";
 import "firebase/firebase-firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsbJbB-RtogNTySTpAaqfrFi4KfwI54Jc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "sendnews-acf7f.firebaseapp.com",
   databaseURL: "https://sendnews-acf7f.firebaseio.com",
   projectId: "sendnews-acf7f",
   storageBucket: "sendnews-acf7f.appspot.com",
   messagingSenderId: "806663170342",
-  appId: "1:806663170342:web:3699ebd4d6218cf62c79e7",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-P0S5KEVR8J"
 };
 
