2d5b2ab32db9d823fad9de7d37db63f1ce1b117d
Sanjay <sunni12@uw.edu>
2019-11-19 18:02:31 -0800

    add style code

--- src/components/Leaderboard/styles.js
+++ src/components/Leaderboard/styles.js
@@ -1,4 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
 
 export const useStyles = makeStyles(theme => ({
   container: {
@@ -7,6 +12,7 @@ export const useStyles = makeStyles(theme => ({
     alignItems: "center",
     flexDirection: "column"
   },
+
   paper: {
     marginTop: theme.spacing(8),
     alignItems: "center"
@@ -15,5 +21,35 @@ export const useStyles = makeStyles(theme => ({
   checkUser: {
     margin: theme.spacing(3),
     alignItems: "center"
-  }
-}));
+  },
+
+
+}))
+
+export const theme = createMuiTheme({
+
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    error: red,
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+    background: {
+      default: "#303030"
+    },
+  },
+
+  typography: {
+    h1: {
+      fontSize: 30,
+      font: "Roboto",
+      fontStyle: "italic"
+    },
+  },
+
+});
