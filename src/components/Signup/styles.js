28a63d653f29e2d1bbf136307eb55f806d0c7d00
pennert-1630865 <pennert@uw.edu>
2019-11-24 16:55:52 -0800

    fixed Login/Signup styling issues

--- src/components/Signup/styles.js
+++ src/components/Signup/styles.js
@@ -1,11 +1,12 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles(theme => ({
-  paper: {
+  card: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    padding: theme.spacing(2),
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1)
   },
