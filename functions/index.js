7bbb2594dc8b8657625eb84133e7f18e9849cb40
Amogh Dave <amoghdave@Amoghs-MacBook-Pro.local>
2019-11-25 19:06:29 -0800

    fixed functions kinda

--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,43 @@ var functions = require('firebase-functions');
 // });
 
 const db = admin.firestore();
-console.log(firebase.db)
 console.log("hello")
-exports.helloWorld = functions.https.onRequest((req,res) =>{
-admin.firestore().collection('news').doc('Document2').get().then(queryResult => {console.log(queryResult.data())
-	return null;
-	}
-);
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+	db.collection("news").get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			// doc.data() is never undefined for query doc snapshots
+			console.log(doc.id, " => ", doc.data());
+		});
+		return null;
+	})
+	.catch(error =>{
+		console.log(error)
+	})
+	res.send("hello world")
+	
+  });
+
+
+// exports.helloWorld = functions.https.onRequest((req,res) =>{
+	// db.collection("news")
+	// .doc("Document2")
+    // .get()
+    // .then(snapshot => {
+    //     snapshot.forEach(doc => {
+	// 		console.log(doc.data)
+    //         const data = {
+    //             id: doc.data,
+    //         }
+    //         authors.push(data)
+    //         authors.push({ key: "test in loop" })
+	// 	})
+		
+	
+// admin.firestore().collection('news').doc('Document2').get().then(queryResult => {console.log(queryResult.data())
+// 	return null;
+// 	}
+// )
 
 // admin.firestore().collection('news/Document2').add({
 // 	      first_name : "babe",
@@ -25,7 +55,15 @@ admin.firestore().collection('news').doc('Document2').get().then(queryResult => 
 //           hobby : "Swimming"
        
 // })
-res.send("Hello World")
+
+
+// admin.firestore().collection('news').doc('Document2').add({
+// 	      first_name : "babe",
+//           last_name : "Chow",
+//           hobby : "Swimming"
+       
+// });
+
 
 
 // db.collection("news").doc("Document2").add({
@@ -36,4 +74,4 @@ res.send("Hello World")
   
 
 	
-});
+// });
