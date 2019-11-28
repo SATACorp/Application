788f856f4b9dac3cc4b444936edb4b9befc5afcd
pennert-1630865 <pennert@uw.edu>
2019-11-27 17:53:00 -0800

    Feed now pulls data directly from firebase, imported LazyLoading component into Feed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
-var admin = require('firebase-admin');
+var admin = require("firebase-admin");
 admin.initializeApp();
-var functions = require('firebase-functions');
-
+var functions = require("firebase-functions");
+const fetch = require("node-fetch");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,39 +11,52 @@ var functions = require('firebase-functions');
 // });
 
 const db = admin.firestore();
-console.log("hello")
+console.log("hello");
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-	db.collection("news").get().then(function(querySnapshot) {
-		querySnapshot.forEach(function(doc) {
-			// doc.data() is never undefined for query doc snapshots
-			console.log(doc.id, " => ", doc.data());
-		});
-		return null;
-	})
-	.catch(error =>{
-		console.log(error)
-	})
-	res.send("hello world")
-	
+let newsUrl =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=0ae0c8f3d55f44f19d14a61a4c1cb105";
+
+fetch(newsUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.articles[0]);
+    return data;
+  })
+  .catch(err => {
+    console.log(err.message);
   });
 
+exports.testFunction = functions.https.onRequest((req, res) => {
+  db.collection("news")
+    .doc("Document2")
+    .get()
+    .then(snapshot => {
+      const data = snapshot.data();
+      res.send(data);
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+  res.send("Test Function");
+});
 
 // exports.helloWorld = functions.https.onRequest((req,res) =>{
-	// db.collection("news")
-	// .doc("Document2")
-    // .get()
-    // .then(snapshot => {
-    //     snapshot.forEach(doc => {
-	// 		console.log(doc.data)
-    //         const data = {
-    //             id: doc.data,
-    //         }
-    //         authors.push(data)
-    //         authors.push({ key: "test in loop" })
-	// 	})
-		
-	
+// db.collection("news")
+// .doc("Document2")
+// .get()
+// .then(snapshot => {
+//     snapshot.forEach(doc => {
+// 		console.log(doc.data)
+//         const data = {
+//             id: doc.data,
+//         }
+//         authors.push(data)
+//         authors.push({ key: "test in loop" })
+// 	})
+
 // admin.firestore().collection('news').doc('Document2').get().then(queryResult => {console.log(queryResult.data())
 // 	return null;
 // 	}
@@ -53,25 +66,20 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
 // 	      first_name : "babe",
 //           last_name : "Chow",
 //           hobby : "Swimming"
-       
-// })
 
+// })
 
 // admin.firestore().collection('news').doc('Document2').add({
 // 	      first_name : "babe",
 //           last_name : "Chow",
 //           hobby : "Swimming"
-       
+
 // });
-
-
 
 // db.collection("news").doc("Document2").add({
 //           first_name : "babe",
 //           last_name : "Chow",
 //           hobby : "Swimming"
 //         })
-  
 
-	
 // });
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,40 +1,46 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import { useStyles } from "./styles";
 import firebase from "../../firebase";
 import NewsCard from "../NewsCard/NewsCard";
 import { CircularProgress } from "@material-ui/core";
+import LazyLoad from "react-lazyload";
 
 export default function Feed() {
   const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState();
 
-  let newsUrl =
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=0ae0c8f3d55f44f19d14a61a4c1cb105";
+  // let newsUrl =
+  //   "https://newsapi.org/v2/top-headlines?country=us&apiKey=0ae0c8f3d55f44f19d14a61a4c1cb105";
 
   useEffect(() => {
-    fetch(newsUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.articles);
-        setNewsArticles(data.articles);
+    let articles = [];
+    firebase.db
+      .collection("news")
+      .get()
+      .then(function(snapshot) {
+        snapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          articles.push(doc.data());
+          console.log(doc.data().url);
+        });
+        setNewsArticles(articles);
       });
   }, []);
 
   if (newsArticles != null) {
-    const newsCards = newsArticles.map((data, index) => 
-    <NewsCard 
-      key={index}
-      title={data.title} 
-      sourceName={data.source.name}
-      urlImage={data.urlToImage}
-      description={data.description}
-    />
-  )
+    const newsCards = newsArticles.map((data, index) => (
+      <NewsCard
+        url={data.url}
+        key={index}
+        title={data.title}
+        sourceName={data.source.name}
+        urlImage={data.urlToImage}
+        description={data.description}
+      />
+    ));
     return (
       <div className={classes.container}>
-        <div>
-          {newsCards}
-        </div>
+        <div>{newsCards}</div>
       </div>
     );
   } else {
@@ -44,5 +50,4 @@ export default function Feed() {
       </div>
     );
   }
-
 }
