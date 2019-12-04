8f0b0eeb258fa85cf6aaf10dffc214e773602dae
pennert-1630865 <pennert@uw.edu>
2019-12-04 14:54:30 -0800

    Added Loader to Profile

--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 250,
+    maxWidth: 250
   },
   container: {
     paddingTop: theme.spacing(12),
@@ -15,20 +15,25 @@ export const useStyles = makeStyles(theme => ({
     margin: theme.spacing(1)
   },
   userCard: {
-    fontSize:20,
-    fontFamily:'Roboto'
+    fontSize: 20,
+    fontFamily: "Roboto"
   },
   pageTitle: {
-    color:"white",
-    fontFamily:"Roboto",
+    color: "white",
+    fontFamily: "Roboto",
     fontSize: 50
   },
   pageSubtitle: {
-    color:"white",
-    fontFamily:"Roboto",
+    color: "white",
+    fontFamily: "Roboto",
     fontSize: 35
   },
   profilePic: {
     width: "50%"
+  },
+  loader: {
+    position: "fixed",
+    top: "50%",
+    left: "50%"
   }
 }));
